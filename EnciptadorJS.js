1660857b8af7f5218b1bddfb11800c4fca53ae33
Uriel <floresruizur13l@outlook.com>
2023-05-28 15:00:42 -0700

    Agregando comentarios al codigo de EncriptadorJS.js

--- EnciptadorJS.js
+++ EnciptadorJS.js
@@ -1,31 +1,25 @@
-//
-//En esta parte del codígo se hace la captura y se muestra el texto.
-//
 const botonEncriptar = document.getElementById("btnEncriptar");
 const botonDesencriptar = document.getElementById("btnDesencriptar");
 const botoncopiar = document.getElementById("btnCopiar");
 var textinput = document.getElementById("txtareaInput"); 
-
+//
+//En esta parte del codígo se muestra el texto.
+//
 function mostrarTexto()
-{   
-    if(textinput.value == "")
-    {
-        alert("Agregue texto antes de hacer cualquier cosa.")
-    }
-    else
-    {
-        mostrarElementos();
-        document.getElementById("Resultado").innerHTML = textinput.value;
-    }
-    
+{    
 }
-
+//
+//Lo que hace esta funcion es mostrar los elementos 
+//que esta ocultos en la section areaMostrarTexto.
+//
 function mostrarElementos(params)
 {
     document.getElementById("Resultado").style.display = "block";
     botoncopiar.style.display = "block";  
 }
-
+//
+//Esta funcion es la encargada de copiar en portapapel 
+//lo que se encuentra en el "Resultado".
 function copiarTexto()
 {
     var elementoACopiar = document.getElementById("Resultado");
@@ -33,13 +27,19 @@ function copiarTexto()
     rango.selectNode(elementoACopiar);
     
     window.getSelection().removeAllRanges(); // Limpiar selecciones previas
-    window.getSelection().addRange(rango);
+    window.getSelection().addRange(rango);//agrega un rango
 
     document.execCommand("copy");
 
     alert("El texto fue copiado.");
 }
-
+//
+//Aqui se ejecutan las funciones.
+//
 botonEncriptar.onclick = mostrarTexto;
 botoncopiar.onclick = copiarTexto;
 botonDesencriptar.onclick = mostrarTexto;
+//
+//Esta Función se encarga de hacer el encriptado.
+//
+
