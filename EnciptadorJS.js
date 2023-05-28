ab9edb4aa0c94f3b6bd920f0f1afc230a47909b9
Uriel <floresruizur13l@outlook.com>
2023-05-28 15:53:03 -0700

    Se creo la funcion Encriptar y le creo el código

--- EnciptadorJS.js
+++ EnciptadorJS.js
@@ -1,20 +1,34 @@
 const botonEncriptar = document.getElementById("btnEncriptar");
 const botonDesencriptar = document.getElementById("btnDesencriptar");
 const botoncopiar = document.getElementById("btnCopiar");
-var textinput = document.getElementById("txtareaInput"); 
+var textinput = document.getElementById("txtareaInput");
+var textoutput = document.getElementById("txtResultado");
 //
 //En esta parte del codígo se muestra el texto.
 //
 function mostrarTexto()
-{    
+{
+
+    if(textinput.value == "")
+    {
+        alert("Agregar texto antes de hacer cualquier cosa.");
+    }
+    else
+    {
+        mostrarElementos();
+        let texto = textinput.value;
+        let txtEncriptado = Encriptar(texto);
+        textoutput.innerHTML = txtEncriptado;
+    }
+
 }
 //
 //Lo que hace esta funcion es mostrar los elementos 
 //que esta ocultos en la section areaMostrarTexto.
 //
-function mostrarElementos(params)
+function mostrarElementos()
 {
-    document.getElementById("Resultado").style.display = "block";
+    document.getElementById("txtResultado").style.display = "block";
     botoncopiar.style.display = "block";  
 }
 //
@@ -22,7 +36,7 @@ function mostrarElementos(params)
 //lo que se encuentra en el "Resultado".
 function copiarTexto()
 {
-    var elementoACopiar = document.getElementById("Resultado");
+    var elementoACopiar = document.getElementById("txtResultado");
     var rango = document.createRange();
     rango.selectNode(elementoACopiar);
     
@@ -31,7 +45,7 @@ function copiarTexto()
 
     document.execCommand("copy");
 
-    alert("El texto fue copiado.");
+    //alert("El texto fue copiado.");
 }
 //
 //Aqui se ejecutan las funciones.
@@ -42,4 +56,45 @@ botonDesencriptar.onclick = mostrarTexto;
 //
 //Esta Función se encarga de hacer el encriptado.
 //
+function Encriptar(cadena)
+{
+    var letrasModificadas = {};
 
+    cadena = cadena.split('').map(function(letra)
+    {
+        if (!['a', 'e', 'i', 'o', 'u'].includes(letra))
+        {
+        return letra;
+        }
+
+        if (letrasModificadas.hasOwnProperty(letra))
+        {
+        return letrasModificadas[letra];
+        }
+
+        var reemplazo = '';
+        switch (letra)
+        {
+        case 'a':
+            reemplazo = 'ai';
+            break;
+        case 'e':
+            reemplazo = 'enter';
+            break;
+        case 'i':
+            reemplazo = 'imes';
+            break;
+        case 'o':
+            reemplazo = 'ober';
+            break;
+        case 'u':
+            reemplazo = 'ufat';
+            break;
+        }
+
+        letrasModificadas[letra] = reemplazo;
+        return reemplazo;
+    }).join('');
+
+    return cadena; 
+}
